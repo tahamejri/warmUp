780f5fae24a03d7ced73afa164fd2b636e0e0387
tahamejri <taha.mejri13@gmail.com>
2019-10-16 10:26:55 +0200

    area of a square

--- warmUp1.js
+++ warmUp1.js
@@ -1,4 +1,9 @@
 // 1-Create a third variable called z, assign x + y to it, and display it.
 var x = 5;
 var y = 10;
+var z = x + y;
+console.log(z);
 // 2-calculate the area of a square using varaiables.
+function squareArea(s){									// 's' is the length of a segment of the square 
+	return 'The area of your square equals = ' + (s*s) ;	// the function returns 's * s' wich is the area of the square
+}
