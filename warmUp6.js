a318b75360dd04921494ee620a8bb8739ba87415
tahamejri <taha.mejri13@gmail.com>
2019-10-23 10:39:19 +0200

    sixth warmup commented

--- warmUp6.js
+++ warmUp6.js
@@ -3,8 +3,8 @@
  mult(3); // => 6
  mult(4); // => 24
  function mult(n) {
- 	var result = 1 ;
- 	for(var i = 1; i<=n; i++){
+ 	var result = 1 ;					// our result will start from 1 cs we have a multiplication
+ 	for(var i = 1; i<=n; i++){			//starting from 1 we increment by 1 till we reach (n) we multiplicate each time
  		result*= i ;
  	}
  	return result ;
@@ -16,11 +16,11 @@
 // eg= 1 2 2 3 3 4 4 5 5 6 6 ...
 
 function string1ToN(num){
-	var count = 2 ;
-	var outStr = '1' ;
-	while(count<=num){
-		outStr += ' ' +count +' '+count ;
-		count++ ;
+	var count = 2 ;				//we set our counter at 2
+	var outStr = '1' ;			//the output string will have the value '1' at the beginning
+	while(count<=num){				
+		outStr += ' ' +count +' '+count ; 	//looping with a while loop from 2 to n we construct our output string
+		count++ ;							//and we increment our counter
 	}
 	return outStr ;
 }
