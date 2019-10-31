f1f65b4d6b46d2837e8c7417ac36be5c01cf4cf1
Anes Mrazouki <marzoukianas@gmail.com>
2019-10-31 10:12:00 +0100

    warmup 12

--- warmUp12.js
+++ warmUp12.js
@@ -9,7 +9,7 @@ Output
     Your function should return the starting (minimum) and ending (maximum) numbers of the range in the form of an array/list comprised of two integers.
     Test Example
 
-    let inputString = '1568141291110137';
+    let inputString = '1568141911210137';
 
     mysteryRange(inputString,10) // [6,15]
 
@@ -22,3 +22,46 @@ Technical Details
     -The maximum size of a range will be 100 integers
     -The starting number of a range will be: 0 < n < 100
  */
+
+ 
+ function randomDig(array){
+ 	var digit = array.splice(Math.floor(Math.random()*10),2) ;
+ 	return digit.join('')
+ }
+ function mysteryRange(str, n){
+ 	var array = str.split('') ;
+ 	var newArr = [] ;
+ 	arrayOfNums = [] ;
+ 	var outputArr = [] ;
+ 	var mini = 0 ;
+ 	var max = 0 ;
+
+ 	 while(array.length + newArr.length > n){
+ 		
+ 	newArr.push(randomDig(array)) ;	
+ 	}
+
+
+ 	array.forEach(function(element){
+ 		newArr.push(element) ;
+ 	})
+
+ 	newArr.forEach(function(element){
+ 		arrayOfNums.push(parseInt(element))
+ 	})
+
+ 	arrayOfNums.forEach(function(element){
+ 		if(element>max){
+ 			max = element ;
+ 		}
+ 		if(element<mini){
+ 			mini = element ;
+ 		}
+
+ 	})
+
+console.log(arrayOfNums) ;
+outputArr.push(mini) ;
+outputArr.push(max) ;
+return outputArr ;
+}
