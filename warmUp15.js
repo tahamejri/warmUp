2f61af306be1052c59d8d1efcef7df0859505b9c
fatima <onlyfefe03@gmail.com>
2019-11-05 09:32:34 +0100

    warmup15

--- warmUp15.js
+++ warmUp15.js
@@ -4,3 +4,28 @@
 //
 //
 // Note : you can`t use any built-in functions .
+
+function reverseStr(str){
+	var arr = [] ;
+	var consStr = ''
+	var newArr = [] ;
+	var cnt =0
+	var result = '' ;
+	for (var i = 0; i<str.length; i++){					//in this stage we will split the string and put it in a new array
+		consStr+=str[i]									//we loop the str and copy its charachters in a new str
+		if(str[i] === ' ' || i === str.length-1){		// and each time we encounter a space we put that constructed string in an array
+			arr[cnt] = consStr ;						
+			consStr = '';
+			cnt++
+		}
+	}
+	for (var j = arr.length-1; j>=0; j--){				//now we make a new array and we fill it with the same values of the above constructed array but reversed
+		var i= arr.length-1 - j ;
+		newArr[i] = arr[j] 
+	}
+	
+	for (var i = 0; i<newArr.length; i++){					//in this stage we join the above array
+		result += newArr[i] + ' '
+	}
+	return result ;
+}
